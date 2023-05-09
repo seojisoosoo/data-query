298a2cf1ee9bc9c11cf84d9645800a2df3803c5e
Seojisoo20191941 <amy0480@naver.com>
2023-05-09 12:53:22 +0900

    [ design ] 갤러리 디자인

--- src/@components/gallery.jsx
+++ src/@components/gallery.jsx
@@ -1,20 +1,62 @@
 import React from "react";
 import { getGalleryInfo } from "../api/galleryInfo";
+import frame from "../assets/frame.svg";
+import { styled } from "styled-components";
+import { theme } from "../style/theme";
 
 export default function gallery(props) {
   const { no, author, material, name, coverThumb } = props;
 
   return (
-    <>
-      <img src={coverThumb} alt={name} />
-      <strong>작품명 | </strong>
-      {name}
-
-      <strong>작가 | </strong>
-      {author}
-
-      <strong>재료 | </strong>
-      {material}
-    </>
+    <GalleryWrapper>
+      <ImgWrapper>
+        <Frame src={frame} alt="프레임" />
+        <Img src={coverThumb} alt={name} />
+      </ImgWrapper>
+      <InfoBox>
+        <p>작품명 | {name}</p>
+        <p>작가 | {author}</p>
+        <p>재료 | {material}</p>
+      </InfoBox>
+    </GalleryWrapper>
   );
 }
+
+const Frame = styled.img`
+  width: 30rem;
+  height: 50rem;
+`;
+
+const Img = styled.img`
+  position: absolute;
+  width: 20rem;
+  height: 24rem;
+  margin: 13rem 0 0 -25rem;
+`;
+
+const GalleryWrapper = styled.article`
+  width: 35rem;
+  height: 45rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImgWrapper = styled.div`
+  width: 30rem;
+  height: 30rem;
+
+  background-color: white;
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 20rem;
+  height: 10rem;
+  background-color: whitesmoke;
+  padding: 3rem;
+  margin: 13rem 0 0 2rem;
+
+  ${({ theme }) => theme.fonts.text}
+`;
