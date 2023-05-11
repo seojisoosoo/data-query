5242f65a22cb8e94b47060fd19b57e9f2c1c2c4c
Seojisoo20191941 <amy0480@naver.com>
2023-05-11 09:10:44 +0900

    [ feat ] api 수정

--- src/api/galleryInfo.jsx
+++ src/api/galleryInfo.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getGalleryInfo() {
   // try {
-  const data = await axios.get(`https://16b9534b-1b6f-4e0a-bd63-b966d5d571f7.mock.pstmn.io/list`);
+  const data = await axios.get(`https://67f09d03-afd1-405f-bac1-4b16e02d1726.mock.pstmn.io/list`);
 
   return data.data;
   // } catch (e) {
